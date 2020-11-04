4a129b37c69f899b76e47862b26ad20c228180c3
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-04 15:52:14 +1100

    with password and key setup

--- tasks/config_check.sh
+++ tasks/config_check.sh
@@ -6,7 +6,6 @@ config=$PT_config
 username=$PT_user
 host=$PT__target
 applymode=$PT_apply_mode
-export
 # diag echos
 echo noop is $PT__noop
 echo host is $PT__target
@@ -34,6 +33,63 @@ else
     apply_command="commit and-quit"
 fi
 
+
+#If we are using password
+if [[ -v PT_password ]]
+then
+    send_command_password()
+    {
+        echo "spawn ssh $username@$newhost"
+        echo "sleep 2"
+        echo "send \r"
+        echo "expect \"Password:\""
+        echo "send \"$PT_password\""
+        echo "expect \"*>\""
+        echo "send \"configure exclusive\r\""
+        echo "expect \"*#\""
+        echo "send \"load $applymode /tmp/boltconfig-$timestamp\r\""
+        echo "expect \"*#\""
+        echo "send \"show | compare\r\""
+        echo "expect \"*#\""
+        echo "send \"$apply_command\r\""
+        if [ $PT__noop == true ]
+        then
+            echo "expect \"*#\""
+            echo "send \"exit configuration-mode\r\""
+        fi
+        echo "expect \"*>\""
+    }
+    send_command_password | /usr/bin/expect -f -
+    exit 0
+fi
+
+#if we are using explicit private key
+if [[ -v PT_ssh_key ]]
+    send_command_explict_key()
+    {
+        echo "spawn ssh -i $PT_ssh_key $username@$newhost"
+        echo "sleep 2"
+        echo "send \r"
+        echo "expect \"*>\""
+        echo "send \"configure exclusive\r\""
+        echo "expect \"*#\""
+        echo "send \"load $applymode /tmp/boltconfig-$timestamp\r\""
+        echo "expect \"*#\""
+        echo "send \"show | compare\r\""
+        echo "expect \"*#\""
+        echo "send \"$apply_command\r\""
+        if [ $PT__noop == true ]
+        then
+            echo "expect \"*#\""
+            echo "send \"exit configuration-mode\r\""
+        fi
+        echo "expect \"*>\""
+    }
+    send_command_explict_key | /usr/bin/expect -f -
+    exit 0
+fi
+
+#assume default ssh key
 send_command()
 {
     echo "spawn ssh $username@$newhost"
@@ -55,8 +111,6 @@ send_command()
     echo "expect \"*>\""
 }
 send_command | /usr/bin/expect -f -
-
-
 
 
 
