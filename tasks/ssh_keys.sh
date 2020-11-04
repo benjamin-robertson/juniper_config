1843a01a27ef45bfd92af26f333ec53063bf1b7e
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-04 12:27:09 +1100

    expect update

--- tasks/ssh_keys.sh
+++ tasks/ssh_keys.sh
@@ -4,11 +4,26 @@ set commitmode [lindex $argv 0]
 set configname [lindex $argv 1]
 set hostname [lindex $argv 2]
 
+proc connect {} {
+  expect {
+    "bolt>" {
+        return 0
+    }
+  }
+  # timeout
+  return 1
+}
+
 spawn ssh bolt@13.211.138.173
-sleep 2
-send "configure exclusive\r"
-send "load set /tmp/boltconfig\r"
-send "show | compare\r"
-send "commit and-quit\r"
-close
-exit
+
+set rez [connect]
+
+if { $rez == 0 } {
+  sleep 2
+  send "configure exclusive\r"
+  send "load set /tmp/boltconfig\r"
+  send "show | compare\r"
+  send "commit and-quit\r"
+  close
+  exit
+}
